d0225a78b6c92c1f8b8800001a87d7f3a0803a6f
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2025-04-08 23:19:40 -0300

    melhore

--- src/components/components/Classification.js
+++ src/components/components/Classification.js
@@ -1,27 +1,26 @@
 import { Text, StyleSheet } from 'react-native';
-import {useState} from 'react';
 
 const Classification = ({imc}) => {
-    let classificação = useState('');
+    let classificação = '';
     if(imc<18.5){
-        classificação = ('Abaixo do peso');
+        classificação = 'Abaixo do peso';
     } else if(imc>18.5 && imc<24.9){
-        classificação = ('Peso normal');
+        classificação = 'Peso normal';
     }else if(imc>25.0 && imc<29.9){
-        classificação = ('Sobrepeso');
+        classificação = 'Sobrepeso';
     }else if(imc>30.0 && imc<34.9){
-        classificação = ('Obesidade grau 1');
+        classificação = 'Obesidade grau 1';
     }else if(imc>35.0 && imc<39.9){
-        classificação = ('Obesidade grau 2');
+        classificação = 'Obesidade grau 2';
     }else if(imc>=40){
-        classificação = ('Obesidade grau 3 (obesidade mórbida)');
+        classificação = 'Obesidade grau 3 (obesidade mórbida)';
     }
     return (
         <Text style={styles.classification}>{classificação}</Text>
     );
 };
 
-const styles = StyleSheet.create({ // estilização do texto para apresentar o resultado do IMC
+const styles = StyleSheet.create({ // estilização do texto para apresentar a classificação do IMC
     classification: {
         marginTop: 20,
         fontSize: 24,
@@ -30,4 +29,4 @@ const styles = StyleSheet.create({ // estilização do texto para apresentar o r
     },
 });
 
-export default Classification; // exportando a função Result
+export default Classification; // exportando a função Classification
