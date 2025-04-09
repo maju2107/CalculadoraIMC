f82f3c06162584cbdedc351f5a852b0e6fb12a2b
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2025-04-08 23:45:54 -0300

    pesos

--- src/components/components/IdealWeight.js
+++ src/components/components/IdealWeight.js
@@ -1,1 +1,25 @@
+import { Text, StyleSheet } from 'react-native';
+import {alturaMetros} from './FormIMC';
 
+const IdealWeight = ({alturaMetros}) => { // função que retorna o valor peso min e max de acordo com a altura em metros
+    const pesoMinimoIdeal = (18.5 * (alturaMetros * alturaMetros)).toFixed(2);
+    const pesoMaximoIdeal = (24.9 * (alturaMetros * alturaMetros)).toFixed(2);
+
+    return ( // exibe os pesos
+        <Text style={styles.idealweight}> 
+            Seu peso mínimo ideal é: {pesoMinimoIdeal} kg ("\n")
+            Seu peso máximo ideal é: {pesoMaximoIdeal} kg
+        </Text>
+    );
+};
+
+const styles = StyleSheet.create({ // estilização do texto para apresentar os pesos
+    idealweight: {
+        marginTop: 20,
+        fontSize: 24,
+        textAlign: 'center',
+        color: '#333',
+    },
+});
+
+export default IdealWeight; // exportando a função IdealWeigth
