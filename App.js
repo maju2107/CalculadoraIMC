e610d7573b066f38700b3855e3e4a8b7226bc42a
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2025-04-09 17:01:46 -0300

    commit

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
 import { StyleSheet, View } from 'react-native';
 import Title from './src/components/components/Title.js'; // Importando a função Title
 import FormIMC from './src/components/components/FormIMC.js'; // Importando a função FormIMC
-import { Dimensions } from 'react-native';
-
-const responsivo = Dimensions.get('window').width;
-
 
 export default function App() { // função que retorna o título do app e o forms para calcular o IMC
   return (
@@ -17,7 +13,6 @@ export default function App() { // função que retorna o título do app e o for
 
 const styles = StyleSheet.create({ // estilização da View(div) que vai conter o título e o forms
   container: {
-    width: responsivo * 0.9,
     flex: 1,
     justifyContent: 'center',
     padding: 20,
