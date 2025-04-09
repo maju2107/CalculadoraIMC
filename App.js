d086ae04ee19ae9e8c85ed3f76d7a0a076eb1778
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2025-04-09 00:01:36 -0300

    js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StyleSheet, View } from 'react-native';
-import Title from './src/components/components/Title'; // Importando a função Title
-import FormIMC from './src/components/components/FormIMC'; // Importando a função FormIMC
+import Title from './src/components/components/Title.js'; // Importando a função Title
+import FormIMC from './src/components/components/FormIMC.js'; // Importando a função FormIMC
 
 export default function App() { // função que retorna o título do app e o forms para calcular o IMC
   return (
