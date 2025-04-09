e0ed01e4d6fd489d2b17549d6a3526e0dccebc5b
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2025-04-08 23:07:23 -0300

    é imc

--- src/components/components/Classification.js
+++ src/components/components/Classification.js
@@ -5,13 +5,13 @@ const Classification = ({imc}) => {
     let classificação = useState('');
     if(imc<18.5){
         classificação = ('Abaixo do peso');
-    } else if(imc>18.5 && im<24.9){
+    } else if(imc>18.5 && imc<24.9){
         classificação = ('Peso normal');
-    }else if(imc>25.0 && im<29.9){
+    }else if(imc>25.0 && imc<29.9){
         classificação = ('Sobrepeso');
-    }else if(imc>30.0 && im<34.9){
+    }else if(imc>30.0 && imc<34.9){
         classificação = ('Obesidade grau 1');
-    }else if(imc>35.0 && im<39.9){
+    }else if(imc>35.0 && imc<39.9){
         classificação = ('Obesidade grau 2');
     }else if(imc>=40){
         classificação = ('Obesidade grau 3 (obesidade mórbida)');
